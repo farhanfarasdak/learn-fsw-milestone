5eea74c45a5b8481911d52b803fd7dadd07a4186
Farhan Farasdak <farhan.farasdak@flash-coffee.com>
2022-06-24 20:25:01 +0700

    Adding container & Jumbotron

--- script.js
+++
@@ -1,8 +0,0 @@
-let jumboContainer = document.querySelector('.jumbo-container');
-let thumbsContainer = document.querySelector('.thumbs-container');
-
-thumbsContainer.addEventListener('click', function(e){
-  if(e.target.className === 'thumb'){
-    jumboContainer.children[0].src = e.target.src
-  }
-})
