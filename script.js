e08b0ad78757412e7310909f502e030d96ffcf2b
Farhan Farasdak <farhan.farasdak@flash-coffee.com>
2022-07-18 16:51:37 +0700

    Tebak Angka

--- script.js
+++ script.js
@@ -1,68 +1,23 @@
-let myContent = document.getElementById("my-content")
-let angka = 0
+alert("Selamat Datang Di Tebak Angka");
+const myPromptName = prompt("Masukkan Nama Anda");
+const myPromptNumber = prompt(`Hi ${myPromptName} berapa angka anda ( 1 - 9 )?`);
 
-const goodNight = () => {
-  let newH2 = document.createElement("h2") // memori3
-  // '<h2></h2>'
-  let myInput = document.getElementById("my-input")
-  newH2.innerText = "Selamat Malam " + myInput.value
-  // <h2>Selamat Malam</h2>
-  newH2.setAttribute("id", "my-custom-h2")
-  newH2.classList.add("simerah")
-  angka++
-  document.getElementById("counter").innerText = angka
-  myContent.append(newH2)
-  newH2.addEventListener("click", () => {
-    newH2.innerText = "Selamat Pagi " + myInput.value
-    newH2.style.color = "blue"
-    myInput.value = ""
-  })
-}
+let myName = document.getElementById("my-name")
+let myNumber = document.getElementById("my-number")
+let randomNumber = document.getElementById("random-number")
+let myResult = document.getElementById("my-result")
+let attempt = 0
 
-let myJudul = document.getElementById("judul")
-myJudul.style.cursor = "pointer"
-myJudul.addEventListener("mouseenter", () => {
-  if(myJudul.innerText === "Hi Semua!"){
-    myJudul.innerText = "Hi Kamu!"
-    myJudul.style.color = "green"
+myName.innerText = myPromptName
+myNumber.innerText = myPromptNumber
+
+const generateNumber = () => {
+  attempt++
+  let number = Math.floor(Math.random() * 10)
+  randomNumber.innerText = number
+  if(number === parseInt(myPromptNumber)){
+    myResult.innerText = `Anda Benar! dalam ${attempt} percobaan`
   }else{
-    myJudul.innerText = "Hi Semua!"
-    myJudul.style.color = "black"
-  }
-})
-
-myJudul.addEventListener("mouseout", () => {
-  if(myJudul.innerText === "Hi Semua!"){
-    myJudul.innerText = "Hi Kamu!"
-    myJudul.style.color = "green"
-  }else{
-    myJudul.innerText = "Hi Semua!"
-    myJudul.style.color = "black"
-  }
-})
-
-
-let searchResults = ["Kura-kura", "Kambing", "Kucing", "Kodok", "Kupu-kupu"]
-
-const generateSearchResults = (keyword) => {
-  for(let i = 0; i < searchResults.length; i++){
-    if(searchResults[i].toLowerCase().includes(keyword)){
-      let myResult = document.getElementById("search-result")
-      let newH3 = document.createElement("h3")
-      newH3.innerText = searchResults[i]
-      myResult.append(newH3)
-    }
+    myResult.innerText = `SUDAH ${attempt}X COBA MASIH SALAH!!!`
   }
 }
-
-generateSearchResults()
-
-let mySearch = document.getElementById("my-search")
-mySearch.addEventListener("keyup", (element) => {
-  // console.log(element.target.value)
-  let myResult = document.getElementById("search-result")
-  myResult.innerHTML = ''
-  if(element.target.value !== ""){
-    generateSearchResults(element.target.value)
-  }
-})
