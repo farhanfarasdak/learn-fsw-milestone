a57b3737c30211ca1232fd578159a94d904bc584
Farhan Farasdak <farhan.farasdak@flash-coffee.com>
2022-08-24 21:02:11 +0700

    Express mongodb

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://farhan-mongodb:farhan123@cluster0.nbn4pfl.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 const express = require('express')
 const app = express()
+const port = 3000
 
-app.set('view engine', 'ejs')
+// READ
+app.get('/engineers', async (req, res) => {
 
-app.use('/css', express.static(__dirname+'/css'))
-app.use('/js', express.static(__dirname+'/js'))
+  let resp
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db =  client.db("myFirstDatabase").collection("engineers");
+  
+  resp = await db.find().toArray()
+  client.close()
 
-
-app.get('/', (req,res)=> {
-  res.send("Hellow World")
+  res.send(resp)
 })
 
-app.get('/pokedex', async (req,res)=> {
-  let pokemonList = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
-  let resultBody = await pokemonList.json()
-  res.render('pokedex', { pokemons: resultBody.results })
+app.get('/engineers/:address', async (req, res) => {
+  let address = req.params.address
+
+  let resp
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db =  client.db("myFirstDatabase").collection("engineers");
+  
+  resp = await db.find({address: address}).toArray()
+  client.close()
+
+  res.send(resp)
 })
 
-app.get('/pokemon', async (req,res)=> {
-  // query params
-  let pokemonName = req.query.name
-  let pokemonDetail = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-  let resultBody = await pokemonDetail.json()
-  res.render('pokemon', { pokemon: resultBody})
-})
+// Create(POST) Read(GET) Update(PUT/PATCH) Delete(DELETE)
 
-app.listen(4000, () => {
-  console.log("Application is running at localhost:4000")
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
