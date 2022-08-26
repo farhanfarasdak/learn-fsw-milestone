d885c610f2919063d2e894d1fc6475a8198b63e3
Farhan Farasdak <farhan.farasdak@flash-coffee.com>
2022-08-26 21:18:11 +0700

    ORM intro

--- index.js
+++ index.js
@@ -1,41 +1,89 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://farhan-mongodb:farhan123@cluster0.nbn4pfl.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 const express = require('express')
+const bodyParser = require('body-parser')
+const { Tweet } = require('./models')
+
 const app = express()
-const port = 3000
+const jsonParser = bodyParser.json()
+
+
+// CRUD = CREATE, READ, UPDATE, DELETE 
 
 // READ
-app.get('/engineers', async (req, res) => {
-
-  let resp
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db =  client.db("myFirstDatabase").collection("engineers");
-  
-  resp = await db.find().toArray()
-  client.close()
-
-  res.send(resp)
+app.get('/', async (req, res)=>{
+  // SELECT * FROM <db_name>;
+  const data = await Tweet.findAll()
+  res.send(data)
 })
 
-app.get('/engineers/:address', async (req, res) => {
-  let address = req.params.address
-
-  let resp
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db =  client.db("myFirstDatabase").collection("engineers");
-  
-  resp = await db.find({address: address}).toArray()
-  client.close()
-
-  res.send(resp)
+app.get('/search', async (req, res) => {
+  // SELECT * FROM <db_name> WHERE name=<>
+  const data = await Tweet.findAll({
+    where: {
+      owner: req.query.name
+    }
+  })
+  res.send(data)
 })
 
-// Create(POST) Read(GET) Update(PUT/PATCH) Delete(DELETE)
+app.get('/:id', async (req, res)=> {
+  // SELECT * FROM <db_name> WHERE id=<id> 
+  const data = await Tweet.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+  if( data == null){
+    res.status(404).send("Data Not Found")
+  }else{
+    res.send(data)
+  }
+})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+// CREATE
+app.post('/', jsonParser, async (req, res)=>{
+  // INSERT INTO <db_name> (col1, col2...) VALUES (val1, val2,...);
+  const data = await Tweet.create({
+    content: req.body.content,
+    owner: req.body.owner
+  })
+  res.send(data)
+})
+
+// UPDATE
+app.put('/:id', jsonParser, async (req, res)=> {
+  try {
+    const data = await Tweet.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+
+    // null.content
+    data.content = req.body.content
+    data.save()
+  
+    res.send(data)
+    
+  } catch (error) {
+    res.status(404).send("Unable to process the request")
+  }
+})
+
+// DELETE
+app.delete('/:id', async (req,res)=>{
+  try {
+    const data = await Tweet.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+    data.destroy()
+    res.send('Data has been deleted')
+  } catch (error) {
+    res.status(404).send("Unable to process the request")
+  }
+})
+
+app.listen(4000, () => {
+  console.log("Running at localhost:4000")
 })
