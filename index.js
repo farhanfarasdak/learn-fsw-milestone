f87f51ec2c22d80a143b7502028e090920b8bc3f
Farhan Farasdak <farhan.farasdak@flash-coffee.com>
2022-09-19 19:55:48 +0700

    async js example

--- index.js
+++ index.js
@@ -1,15 +1,106 @@
-const express = require('express')
-const passport = require('passport')
-require('./utils/passport')
-const apiRouter = require('./routes/api')
+// Callback
 
-const app = express()
-const port = 8989
 
-app.use(passport.initialize())
+// setTimeout(
 
-app.use(apiRouter)
+//   () => { 
+//     console.log("HEHE") 
+//     console.log("HIHI")
+//   }, // this is callback
+  
+//   2000
+// )
 
-app.listen(port, () => {
-  console.log(`App is running at port ${port}`)
-})
+// $(".button").click(
+//   () => {
+//     console.log("I am clicked")
+//   }
+// )
+
+// const fs = require('fs')
+
+// fs.readFile('./test.txt', { encoding: 'utf-8'}, (err, data) => {
+//   if(err){
+//     console.log(err)
+//   }else{
+//     setTimeout(
+//       () => {
+//         setTimeout(
+//           () => {
+//             setTimeout(
+//               () => {
+//                 console.log("wow")
+//               },
+//               1000
+//             )
+//           },
+//           1000
+//         )
+//       },
+//       2000
+//     )
+//   }
+  
+// } )
+// console.log("HEHE")
+
+// callback hell
+
+
+
+
+// Promise
+// resolve - reject
+
+// const myPromise = new Promise((resolve, reject) => {
+//   const random = Math.floor(Math.random() * 2) // 0 - 1
+//   if(random === 0){
+//     resolve(`Resolved With Value ${random}`)
+//   }else{
+//     reject(`Rejected with Value ${random}`)
+//   }
+// })
+
+// myPromise
+//   .then( (a) => { console.log(`THEN - ${a}`) } )
+//   .catch( (b) => { console.log(`CATCH - ${b}`) } )
+
+
+// const fs = require('fs')
+
+// fs.promises.readFile('./test.txt', { encoding: 'utf-8' })
+//   .then( (data) => { console.log(data) })
+//   .catch( (error) => { console.log(error) })
+
+// fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+//   .then( (resp) => {
+//     resp.json()
+//     .then( (data) => {
+//       console.log(data)
+//     })
+//   })
+//   .catch( (err) => {
+//     console.log('WITHIN CATCH')
+//     console.log(err)
+//   })
+
+
+// Async - Await
+
+// const fs = require('fs')
+
+// const loadData = async () => {
+//   const data = await fs.promises.readFile('./test.txt', {encoding: 'utf-8'})
+//   console.log(data)
+// }
+
+// loadData()
+
+// const getPokemonData = async () => {
+//   const pokemonData = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+//   const bodyResponse = await pokemonData.json()
+//   console.log(bodyResponse)
+// }
+
+// getPokemonData()
+
