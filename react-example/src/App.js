89d56556e8b56210a19f6db88d73b1eb2c403812
Farhan Farasdak <farhan.farasdak@flash-coffee.com>
2022-10-24 15:15:18 +0200

    create boiler plate

---
+++ react-example/src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import './App.css';
+import Home from './pages/home';
+import About from './pages/about';
+
+
+function App() {
+  return (
+    <h1>Boiler Plate</h1>
+  );
+}
+
+export default App;
